df27c1be417fa17e94b6f062f5e5575ef1244e1e
Lucas Lima <lucas.limamp@outlook.com>
2023-08-16 19:51:05 -0300

    !

--- Atividades/src/Exercícios/Exe1.java
+++ Atividades/src/Exercícios/Exe1.java
@@ -1,17 +1,17 @@
 package Exercícios;
 
-public class Exe1 {
-    public static void main(String[] args) {
-        Integer a = 10;
-        Integer b = 20;
-        
-        trocavalor(a, b);
-        System.out.println("Valor de A: " + a + " Valor de B: " + b);
-    }
-    
-    public static void trocavalor(Integer a, Integer b) {
-        Integer aux = a;
+public class Exe1 {        
+    public static void troca(){
+        int a = 10;
+        int b = 20;
+        int aux = a;
         a = b;
         b = aux;
+        System.out.println("O valor de A: "+a+"\nO valor de B: "+b);
     }
+    public static void main(String[]args){
+        Exe1 obj1 = new Exe1(); // Instanciando o objeto
+        obj1.troca(); //Chamando o método através do objeto instanciado 
+    }
+
 }
