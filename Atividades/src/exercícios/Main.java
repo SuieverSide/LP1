2f71588ce36a88b4228081f58cc2a6914a47c9c6
Lucas Lima <31674205+SuieverSide@users.noreply.github.com>
2023-08-22 21:53:03 -0300

    Delete Atividades/src/exercícios directory

--- Atividades/src/exercícios/Main.java
+++
@@ -1,9 +0,0 @@
-package exercícios;
-import exercícios.Livro;
-public class Main{  
-    public static void main(String[]args){
-        Livro obj1 = new Livro(); // Estou instanciando o objeto
-        obj1.setId(45); // Buscando o atributo e atribuindo um valor a ele
-        System.out.println(obj1.getId()); //Buscando o valor 
-    }
-}
