5c2960c86bf1ce0fb47f2e060b3edfeeef7f5095
Lucas Lima <lucas.limamp@outlook.com>
2023-08-22 21:55:07 -0300

    2

---
+++ Atividades/src/Exercícios/Exe5.java
@@ -0,0 +1,12 @@
+package exercícios;
+import java.util.Scanner;
+public class Exe5 {
+    public static int antecessor(int valor){
+        return(valor-1);
+    }
+    public static void main(String[]args){
+        Scanner sc1 = new Scanner(System.in);
+        System.out.print("Digite um valor: ");
+        System.out.println("O valor anterior é: "+antecessor(+sc1.nextInt())); //O digitado é atribuido vai para o antecessor 
+    }
+}
